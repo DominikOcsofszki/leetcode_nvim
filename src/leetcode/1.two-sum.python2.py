729d88a12c2adf99befa3f30b3a1b23b7293ef0b
Dominik Ocsofszki <dominikocsofszki@gmail.com>
2024-11-15 15:33:18 +0100

    Add pdm

---
+++ src/leetcode/1.two-sum.python2.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+# @leet start
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+    # @leet end
+
+
+def test_twoSum(a, b):
+    assert a == b
---
+++ src/leetcode/274.h-index.python2.py
@@ -0,0 +1,9 @@
+# @leet start
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        
+# @leet end
